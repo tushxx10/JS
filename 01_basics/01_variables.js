10da4d17ee1ea8e613dec59b605c035d77a86326
tushxx10 <tusharg0910@gmail.com>
2024-06-03 23:27:26 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144563
+let accountEmail = "tusharg0910@google.com"
+var accountPassword = "tushxx10"
+accountCity= "Delhi"
+let accountState;
+
+
+// accountId = 2   //not Allowed
+
+accountEmail="sbq@nwj.com"
+accountPassword="1234"
+accountCity= "Mumbai"
+
+console.log(accountId);
+
+
+
+/*
+Prefer not to use (var)
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
