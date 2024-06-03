f2b3c6a1c8ab4c56e10b97e1899c4f3eac32bfbe
tushxx10 <tusharg0910@gmail.com>
2024-06-03 23:59:06 +0000

    dataType

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS cade as newer version 
+
+// alert(3+3) //we are using nodejs not browser
+
+console.log(3
+    +
+    3)  //codde readibility should be high
+console.log("Tushar")
+
+let name="Tushar"
+let age=21
+let LoggedIn=false
+let state;
+
+// Number => 2 to power 53
+//bigint  (for very big nos)
+//string=> can be used in both single and double inverted commaas
+//boolean =>   true/false
+//null => standalone value
+//undefined =>  abhi value assign nhi hui h
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
