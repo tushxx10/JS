152221fd1dafbcc40e2e20f4034765cd8bd03822
tushxx10 <tusharg0910@gmail.com>
2024-06-04 04:20:32 +0000

    comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+
+// console.log("1">2);
+// console.log("02" > 1);
+
+
+// avoid these kinda comparisons
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+//  tripple check(===) it also checks the data type with checking the value
+
+console.log("2"===2); //false as their data types are different
+
+
