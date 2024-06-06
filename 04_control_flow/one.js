eacc8f20149b3a401da9c7611585811f2aec7460
tushxx10 <tusharg0910@gmail.com>
2024-06-06 21:00:57 +0000

    IfElse

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+// if statement
+// if(true){
+
+// }
+// if(false){
+
+// }
+
+// const isUserLoggedIn= true
+// const temp = 41
+// if(temp<50){ 
+//     console.log("temp less than 50")
+// }
+// else{
+//     console.log("temp greater than 50")
+// }
+
+// console.log("Executed")
+
+//checking operators : >,<,>=,<=,==,===,!=,!==
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(` User power : ${power}`)
+// }
+// console.log(` User power : ${power}`)  //this will not run kyuki scope ke bahar hai
+
+//if(score > 150){
+//    var power = "fly"  //scope of var is completely global
+//    console.log(` User power : ${power}`)
+//}
+// console.log(` User power : ${power}`) //abb yeh chal rha h kyuki (VAR) ka use hua h
+
+
+//Short  hand notation
+
+// const balance = 1000
+// if(balance > 500) console.log("YES"),
+// console.log("YES1");           //not a good way of writing code
+
+// if( balance < 500){
+//     console.log("less than 500")
+// } else if(balance <750){
+//     console.log("less than 750")
+// } else if(balance < 900){
+//     console.log("less than 900")
+// } else{
+//     console.log("less than 1200")
+// }
+
+
