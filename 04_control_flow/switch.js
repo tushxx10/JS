868e8f07a805781a3fdf2422ee241c2ea83f1ee8
tushxx10 <tusharg0910@gmail.com>
2024-06-06 21:01:10 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+//Switch case statement
+//Syntax (we check multiple values in it)
+
+// switch(key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch(month){
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    case 5:
+        console.log("may")
+        break;
+
+        default:
+            console.log("default case")
+            break;
+}
+
