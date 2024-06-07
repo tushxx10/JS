5e5585b416ec71e23a4f95eeb0f3d76c4dacb488
tushxx10 <tusharg0910@gmail.com>
2024-06-07 19:47:24 +0000

    whileloop

---
+++ 05_Iterations/WhileLoop.js
@@ -0,0 +1,22 @@
+//While Loop
+
+let index=0
+while (index <= 10) {
+    //console.log(`Value of index is ${index}`);
+    index +=2;
+}
+
+let myArray = ['Flash' , 'Superman' , 'Spiderman']
+let arr = 0
+while(arr < myArray.length){
+    //console.log(`Value of array is ${myArray[arr]}`);
+    arr++;
+}
+
+//DO WHILE Loop
+
+let score = 1;
+do {
+    console.log(`Value is ${score}`);
+    score+=1;
+} while (score <= 10);
